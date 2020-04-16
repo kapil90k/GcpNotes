f45554a65d52b4230a99656fa27648c515f6df74
Kumar <kapil.h.kumar@accenture.com>
2020-04-16 17:40:58 +0530

    BQ/BQML/KUBERNET

---
+++ BQ/Array,Structs.sql
@@ -0,0 +1,66 @@
+SELECT ['raspberry', 'blackberry', 'strawberry', 'cherry'] AS fruit_array
+
+#standardSQL
+SELECT person, fruit_array, total_cost FROM `data-to-insights.advanced.fruit_store`;
+
+SELECT
+  fullVisitorId,
+  date,
+  v2ProductName,
+  pageTitle
+  FROM `data-to-insights.ecommerce.all_sessions`
+WHERE visitId = 1501570398
+ORDER BY date
+
+SELECT
+  fullVisitorId,
+  date,
+  ARRAY_AGG(v2ProductName) AS products_viewed,
+  ARRAY_AGG(pageTitle) AS pages_viewed
+  FROM `data-to-insights.ecommerce.all_sessions`
+WHERE visitId = 1501570398
+GROUP BY fullVisitorId, date
+ORDER BY date
+
+
+SELECT
+  fullVisitorId,
+  date,
+  ARRAY_AGG(v2ProductName) AS products_viewed,
+  ARRAY_LENGTH(ARRAY_AGG(v2ProductName)) AS num_products_viewed,
+  ARRAY_AGG(pageTitle) AS pages_viewed,
+  ARRAY_LENGTH(ARRAY_AGG(pageTitle)) AS num_pages_viewed
+  FROM `data-to-insights.ecommerce.all_sessions`
+WHERE visitId = 1501570398
+GROUP BY fullVisitorId, date
+ORDER BY date
+
+
+SELECT
+  fullVisitorId,
+  date,
+  ARRAY_AGG(DISTINCT v2ProductName) AS products_viewed,
+  ARRAY_LENGTH(ARRAY_AGG(DISTINCT v2ProductName)) AS distinct_products_viewed,
+  ARRAY_AGG(DISTINCT pageTitle) AS pages_viewed,
+  ARRAY_LENGTH(ARRAY_AGG(DISTINCT pageTitle)) AS distinct_pages_viewed
+  FROM `data-to-insights.ecommerce.all_sessions`
+WHERE visitId = 1501570398
+GROUP BY fullVisitorId, date
+ORDER BY date
+
+
+STRUCT--->
+
+SELECT
+  visitId,
+  totals.*,
+  device.*
+FROM `bigquery-public-data.google_analytics_sample.ga_sessions_20170801`
+WHERE visitId = 1501570398
+LIMIT 10
+
+
+#standardSQL
+SELECT STRUCT("Rudisha" as name, 23.4 as split) as runner
+
+gs://data-insights-course/labs/optimizing-for-performance/race_results.json
